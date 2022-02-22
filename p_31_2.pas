cf1f6f6b4e8b4e48a69eb943855d2f890ad98fa4
Anton Pashkin <anton.zep@gmail.com>
2022-02-22 20:59:11 +0300

    chapter 31

---
+++ p_31_2.pas
@@ -0,0 +1,103 @@
+{
+v.2
+Поиск по базе данных угнанных автомобилей,
+содержащейся в исходном файле 'police2.txt',
+с выводом информации об автомобиле, в случае, если автомобиль найден.
+Исходный файл содержит 3 поля в строке:
+номер автомобиля    марка автомобиля    регион
+Исходный файл содержит Unicode-символы.
+}
+
+{$H+}
+{$codepage UTF8}
+
+uses
+{$IFNDEF WINDOWS}
+    cwstring;
+{$ENDIF}
+
+const
+    dbFileName = 'police2.txt';
+
+function FindNumber(var dbFile : text; var num : UnicodeString) : boolean;
+var
+    str : UnicodeString;
+    ch : UnicodeChar;
+    i : integer;
+begin
+    str := '';
+    FindNumber := false;
+    reset(dbFile);
+    while (not SeekEof(dbFile)) do begin
+        for i := 1 to 8 do
+            if (not SeekEoln(dbFile)) then begin
+                read(dbFile, ch);
+                str := str + ch;
+            end;
+        read(dbFile, ch);
+        if ((Ord(ch) > 47) and (Ord(ch) < 58)) then
+            str := str + ch;
+        if (str = num) then begin
+            FindNumber := true;
+            break;
+        end else if (not SeekEof(dbFile)) then begin
+            readln(dbFile);
+            str := '';
+        end;
+    end;
+end;
+
+procedure GetAdditInfo(
+    var dbFile : text;
+    var wantedNum, wantedModel, wantedArea  : UnicodeString
+);
+var
+    tempStr : UnicodeString;
+    i, j : integer;
+begin
+    wantedModel := '';
+    wantedArea := '';
+    if (FindNumber(dbFile, wantedNum)) then begin
+        if (not SeekEoln(dbFile)) then
+            readln(dbFile, tempStr);
+
+        for i := 1 to length(tempStr) do begin 
+            if (Ord(tempStr[i]) = 32) then break;
+            wantedModel := wantedModel + tempStr[i];
+        end;
+        
+        for j := i + 1 to length(tempStr) do
+            wantedArea := wantedArea + tempStr[j];
+    end;
+end;
+
+{ main }
+var
+    dbFile : text;
+    autoNum, model, area : UnicodeString;
+begin
+    assign(dbFile, dbFileName);
+    {$I-}
+        reset(dbFile);
+    {$I+}
+    if (IOResult = 0) then begin
+        writeln('Input vehicle number to search or 0 to exit.');
+        repeat
+            write('Vehicle number: ');
+            readln(autoNum);
+            if (autoNum = '0') then break;
+            reset(dbFile);
+            if (FindNumber(dbFile, autoNum) = true) then begin
+                writeln('Vehicle is wanted! ');
+                GetAdditInfo(dbFile, autoNum, model, area);
+                writeln(#9'Марка: ', model);
+                writeln(#9'Регион: ', area);
+            end else begin
+                writeln('No records found.');
+            end
+        until (autoNum = '0');
+        close(dbFile);
+    end else begin
+        writeln('File ', dbFileName,' not found!');
+    end;
+end.
---
+++ p_31_3.pas
@@ -0,0 +1,137 @@
+{
+Файл с физическими данными старшеклассников содержит три колонки:
+фамилия, рост и вес ученика. 
+Создайте программы для решения следующих задач:
+· отбор кандидатов для занятий баскетболом, – рост кандидата должен
+составлять не менее 175 см;
+· поиск учеников с избыточным весом, для которых разница между ростом
+ученика (см) и его весом (кг) составляет менее 100.
+Ваши программы должны сформировать соответствующие файлы с
+фамилиями и данными учеников.
+Имя исходного файла: 'studentsPD.txt'
+}
+
+{$mode objfpc}
+{$H+}
+{$codepage UTF8}
+
+uses
+{$IFNDEF WINDOWS}
+    cwstring, 
+{$ENDIF}
+    SysUtils;
+
+const
+    inFileName = 'studentsPD.txt';
+    outFileNameCand = 'studentsCand.txt';
+    outFileNameHealth = 'studentsHealth.txt';
+
+procedure SplitStr(
+    var inStr, outFam : UnicodeString;
+    var outHeight, outWeight : integer
+);
+var
+    i : integer;
+    tempStr : UnicodeString;
+begin
+    tempStr := '';
+    outFam := '';
+    outHeight := 0;
+    outWeight := 0;
+    for i := 1 to length(inStr) do begin
+        if (Ord(inStr[i]) = 32) then
+            if (outFam = '') then continue
+            else break;
+        outFam := outFam + inStr[i];
+    end;
+
+    for i := i to length(inStr) do begin
+        if (Ord(inStr[i]) = 32) then
+            if (tempStr = '') then continue
+            else break;
+        if ((Ord(inStr[i]) > 47) and (Ord(inStr[i]) < 58)) then
+            tempStr := tempStr + inStr[i];
+    end;
+    if (tempStr <> '') then
+        outHeight := StrToInt(tempStr);
+    tempStr := '';
+
+    for i := i to length(inStr) do begin
+        if (Ord(inStr[i]) = 32) then
+            if (tempStr = '') then continue
+            else break;
+        if ((Ord(inStr[i]) > 47) and (Ord(inStr[i]) < 58)) then
+            tempStr := tempStr + inStr[i];
+    end;
+    if (tempStr <> '') then
+        outWeight := StrToInt(tempStr);
+end;
+
+
+procedure GetCandidates(const inFileName, outFileName : string);
+var
+    inFile, outFile : text;
+    str, stFam : UnicodeString;
+    stHeight, stWeight : integer;
+begin
+    assign(inFile, inFileName);
+    {$I-}
+        reset(inFile);
+    {$I+}
+    If (IOResult = 0) then begin
+        assign(outFile, outFileName);
+        rewrite(outFile);
+        { title }
+        if (not SeekEof(inFile)) then begin
+            writeln(outFile, 'Ученики с ростом не менее 175 см');
+            readln(inFile, str);
+            writeln(outFile, str);
+        end;
+        while (not SeekEof(inFile)) do begin
+            readln(inFile, str);
+            SplitStr(str, stFam, stHeight, stWeight);
+            if (stHeight >= 175) then
+                writeln(outFile, str);
+        end;
+        close(inFile);
+        close(outFile);
+    end else
+        writeln('Error! File ', inFileName, ' not found!');
+end;
+
+procedure GetHealth(const inFileName, outFileName : string);
+var
+    inFile, outFile : text;
+    str, stFam : UnicodeString;
+    stHeight, stWeight : integer;
+begin
+    assign(inFile, inFileName);
+    {$I-}
+        reset(inFile);
+    {$I+}
+    If (IOResult = 0) then begin
+        assign(outFile, outFileName);
+        rewrite(outFile);
+        { title }
+        if (not SeekEof(inFile)) then begin
+            writeln(outFile, 'Ученики с избыточным весом');
+            readln(inFile, str);
+            writeln(outFile, str);
+        end;
+        while (not SeekEof(inFile)) do begin
+            readln(inFile, str);
+            SplitStr(str, StFam, stHeight, stWeight);
+            if ((stHeight - stWeight) < 100) then
+                writeln(outFile, str);
+        end;
+        close(inFile);
+        close(outFile);
+    end else
+        writeln('Error! File ', inFileName, ' not found!');
+end;
+
+{ main }
+begin
+    GetCandidates(inFileName, outFileNameCand);
+    GetHealth(inFileName, outFileNameHealth);
+end.
